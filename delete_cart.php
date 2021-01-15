66546c19dd7c9b2a6df778a8b73ed882b36e9f46
Daniel Gaik <r3corddd@gmail.com>
2021-01-16 00:44:02 +0100

    Fixed redirecting when making changes from cart page

--- delete_cart.php
+++ delete_cart.php
@@ -3,7 +3,14 @@ session_start();
 if(isset($_POST['rest_name'])) //Sprawdzenie czy wymagane zmienne są ustalone
 {
 	unset($_SESSION['cart']); //Usunięcie tablicy, która jest koszykiem
-	header('Location: dishes_list.php?rest='.$_POST['rest_name']); //Powrót do prawidłowej strony
+	if($_POST['rest_name']!="cart.php") //Sprawdzenie czy aktualizacja jest robiona z korzyka czy nie
+	{
+		header('Location: dishes_list.php?rest='.$_POST['rest_name']); //Powrót do odpowiedniej strony
+	}
+	else
+	{
+		header('Location: cart.php');
+	}
 	exit();
 }
 else
--- update_quantity.php
+++ update_quantity.php
@@ -5,7 +5,14 @@ if(isset($_POST['row']) && isset($_POST['quantity']) && isset($_POST['rest_name'
 {
 	$_SESSION['cart'][$_POST['row']][3]=$_POST['quantity']; //Aktualizacja ilości
 	$_SESSION['cart'][$_POST['row']][5]=$_SESSION['cart'][$_POST['row']][4]*$_SESSION['cart'][$_POST['row']][3];
-	header('Location: dishes_list.php?rest='.$_POST['rest_name']); //Powrót do prawidłowej strony
+	if($_POST['rest_name']!="cart.php") //Sprawdzenie czy aktualizacja jest robiona z korzyka czy nie
+	{
+		header('Location: dishes_list.php?rest='.$_POST['rest_name']); //Powrót do odpowiedniej strony
+	}
+	else
+	{
+		header('Location: cart.php');
+	}
 	exit();
 }
 else
