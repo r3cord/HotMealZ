82b84679df6329c0cd3b278c65c2222d63c9627d
Daniel Gaik <r3corddd@gmail.com>
2021-01-15 19:10:37 +0100

    Fixed price calculation
    Fixed price calculation when updating quantity

--- update_quantity.php
+++ update_quantity.php
@@ -4,6 +4,7 @@ session_start();
 if(isset($_POST['row']) && isset($_POST['quantity']) && isset($_POST['rest_name'])) //Sprawdzenie czy wymagane zmienne są ustalone
 {
 	$_SESSION['cart'][$_POST['row']][3]=$_POST['quantity']; //Aktualizacja ilości
+	$_SESSION['cart'][$_POST['row']][5]=$_SESSION['cart'][$_POST['row']][4]*$_SESSION['cart'][$_POST['row']][3];
 	header('Location: dishes_list.php?rest='.$_POST['rest_name']); //Powrót do prawidłowej strony
 	exit();
 }
